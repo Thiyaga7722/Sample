4c7caa77d20766f4a09161969903ed07d46f03fd
Thiyaga7722 <thiyagamcc1989gmail.com>
2020-03-08 21:27:50 +0530

    design

---
+++ src/main/java/org/acc/testcase_7/batch/App.java
@@ -0,0 +1,13 @@
+package org.acc.testcase_7.batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/org/acc/testcase_7/batch_gitdemo/Gitdemo.java
@@ -0,0 +1,8 @@
+package org.acc.testcase_7.batch_gitdemo;
+
+public class Gitdemo {
+	public static void main(String[]arg) {
+		System.out.println("thiaga");
+	}
+
+}
